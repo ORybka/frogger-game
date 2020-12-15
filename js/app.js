ea9796a4e3a99acff53e4b9b744f80e96cbbb9f4
Olha Rybchynska <olha.rybchynska@gmail.com>
2020-12-15 13:34:14 +0200

    add speed for enemies

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-const fieldBorders = {
+const parameters = {
   fieldBorderX: [0, 400],
   fieldBorderY: [0, 400],
+  enemyX: -50,
+  enemyY: [60, 145, 230],
+  maxSpeed: 200,
+  minSpeed: 50,
+  enemyHeight: 70,
+  enemyWidth: 70,
 };
 
 class Enemy {
@@ -8,16 +14,14 @@ class Enemy {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = Math.floor(Math.random() * 40);
+    this.speed = Math.floor(Math.random() * (parameters.maxSpeed - parameters.minSpeed) + parameters.minSpeed);
   }
 
-  // Update the enemy's position, required method for game
-  // Parameter: dt, a time delta between ticks
   update(dt) {
-    this.dt = dt;
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += this.speed * dt;
+    if (this.x >= parameters.fieldBorderX[1] + parameters.enemyWidth) {
+      this.x = parameters.enemyX;
+    }
   }
 
   render() {
@@ -30,9 +34,6 @@ class Player {
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
-    this.enemy = allEnemies;
-    this.enemyHeight = 70;
-    this.enemyWidth = 80;
   }
 
   update() {
@@ -47,27 +48,27 @@ class Player {
     switch (position) {
       case 'left':
         this.x -= 100;
-        if (this.x <= fieldBorders.fieldBorderX[0]) {
-          this.x = fieldBorders.fieldBorderX[0];
+        if (this.x <= parameters.fieldBorderX[0]) {
+          this.x = parameters.fieldBorderX[0];
         }
         break;
       case 'up':
         this.y -= 80;
-        if (this.y <= fieldBorders.fieldBorderY[0]) {
+        if (this.y <= parameters.fieldBorderY[0]) {
           this.y = -30;
           this.winMessage();
         }
         break;
       case 'right':
         this.x += 100;
-        if (this.x >= fieldBorders.fieldBorderX[1]) {
-          this.x = fieldBorders.fieldBorderX[1];
+        if (this.x >= parameters.fieldBorderX[1]) {
+          this.x = parameters.fieldBorderX[1];
         }
         break;
       case 'down':
         this.y += 80;
-        if (this.y >= fieldBorders.fieldBorderY[1]) {
-          this.y = fieldBorders.fieldBorderY[1] - 20;
+        if (this.y >= parameters.fieldBorderY[1]) {
+          this.y = parameters.fieldBorderY[1] - 20;
         }
         break;
       default:
@@ -75,15 +76,9 @@ class Player {
     }
   }
 
-  // checkCollision() {
-  //   if (this.y <= this.enemy.y + this.enemyHeight && this.y >= this.enemy.y - this.enemyHeight && this.x <= this.enemy.x + this.enemyWidth && this.x >= this.enemy.x - this.enemyWidth) {
-  //     this.loseMessage();
-  //   }
-  // }
-
   checkCollision() {
-    for (let i = 0; i < this.enemy.length; i++) {
-      if (this.y <= this.enemy[i].y + this.enemyHeight && this.y >= this.enemy[i].y - this.enemyHeight && this.x <= this.enemy[i].x + this.enemyWidth && this.x >= this.enemy[i].x - this.enemyWidth) {
+    for (let i = 0; i < allEnemies.length; i++) {
+      if (this.y <= allEnemies[i].y + parameters.enemyHeight && this.y >= allEnemies[i].y - parameters.enemyHeight && this.x <= allEnemies[i].x + parameters.enemyWidth && this.x >= allEnemies[i].x - parameters.enemyWidth) {
         this.loseMessage();
       }
     }
@@ -109,11 +104,8 @@ class Player {
   }
 }
 
-const enemyX = 0;
-const enemyY = [60, 145, 230];
-
 const allEnemies = [];
-enemyY.forEach((el) => allEnemies.push(new Enemy(enemyX, el)));
+parameters.enemyY.forEach((el) => allEnemies.push(new Enemy(parameters.enemyX, el)));
 
 const player = new Player(200, 380);
 
